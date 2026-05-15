580318e51802ad7bb4363685e4ec44f9c04b6495
nitin <nitin127001@gmail.com>
2026-05-15 13:56:44 +0530

    feat: Initialize Spring Boot project with Gradle wrapper
    - Added gradle-wrapper.properties for Gradle 9.4.1 configuration.
    - Included gradlew and gradlew.bat scripts for executing Gradle tasks.
    - Created settings.gradle to define the root project name as 'spring-lib-prcte'.
    - Implemented main application class SpringLibPrcteApplication.
    - Configured application name in application.yaml.
    - Added a test class SpringLibPrcteApplicationTests with a basic context load test.

---
+++ src/main/java/com/spring/spring_lib_prcte/SpringLibPrcteApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring_lib_prcte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibPrcteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibPrcteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/spring_lib_prcte/SpringLibPrcteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring_lib_prcte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibPrcteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
